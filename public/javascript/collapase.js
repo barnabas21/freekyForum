aa6b3feeed3064a0b4c764a91a1a2885e5cadbc4
Barnabas <byahlom175@gmail.com>
2020-05-14 23:56:52 +0000

    final1

--- public/javascript/collapase.js
+++ public/javascript/collapase.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("mySidebar").style.width = "300px";
     document.getElementById("main").style.marginLeft = "250px";
   }
   
